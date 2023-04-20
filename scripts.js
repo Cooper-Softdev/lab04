86dcf739d13d9f63d1d240f295a467a1a04b5e2d
Cooper <cooper.softdev@gmail.com>
2023-04-19 22:34:36 -0800

    like wtf seriously

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+const strawberrybutton = document.getElementById("strawberryButton");
+strawberrybutton.addEventListener('click', function() {
+  alert('oh WOW! Mine too! Now we are friends! Forever');
+});
+
+const vanillabutton = document.getElementById("vanillaButton");
+vanillabutton.addEventListener('click', function() {
+  alert('WRONG WRONG WRONG! Are you trying to make me angry?');
+});
+
+const chocolatebutton = document.getElementById("chocolateButton");
+chocolatebutton.addEventListener('click', function() {
+  alert('If you answer wrong again... its all over for you.');
+});
+
+const usersName = prompt("Hi! I have been so lonely I could use a friend, what is your name?");
+
